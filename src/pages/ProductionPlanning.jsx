4f1343d3eb5ed7167e47130e963148cc30d3c480
Amar Kahlon <info@nuvirajuice.com>
2026-04-24 17:57:52 +0000

    File changes

--- src/pages/ProductionPlanning.jsx
+++ src/pages/ProductionPlanning.jsx
@@ -130,7 +130,7 @@ export default function ProductionPlanning() {
                   type="date"
                   value={dateFrom}
                   onChange={e => setDateFrom(e.target.value)}
-                  className="w-44"
+                  className="w-44 bg-green-50 border-green-200"
                 />
               </div>
               <div className="space-y-1">
@@ -139,7 +139,7 @@ export default function ProductionPlanning() {
                   type="date"
                   value={dateTo}
                   onChange={e => setDateTo(e.target.value)}
-                  className="w-44"
+                  className="w-44 bg-green-50 border-green-200"
                 />
               </div>
               <div className="flex gap-2">
